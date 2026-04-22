e938214a56d93a51d667a737b502714be70264c1
Aryan Bhandari <your_github_email>
2026-04-23 00:01:18 +0530

    Saving pending changes

--- main.py
+++ main.py
@@ -108,4 +108,8 @@ def verify_coupon(data: CouponVerify):
         "valid": True,
         "discount": coupon["discount"],
         "type": "percentage"
+<<<<<<< HEAD
     }
+=======
+    }
+>>>>>>> d14a68d (Saving pending changes)
